8324dee1fff55cb876ed32bc07d5e183158d130a
˹ꝛᴅꭙ ꝛᴀj˼ <159524633+RDX-RAj@users.noreply.github.com>
2026-07-15 00:56:22 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 
+window.addEventListener("load", () => {
+  console.log("Editor Raj Website Loaded Successfully!");
+});
+
+document.querySelectorAll("nav a").forEach(link => {
+  link.addEventListener("click", e => {
+    console.log("Menu Clicked:", e.target.textContent);
+  });
+});
